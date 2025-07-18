7994d1218c5554d8371c9a9c21d06f3f3195f441
Siddhesh kshirsagar <siddheshkshirsagar20@gmail.com>
2025-07-18 16:15:41 +0530

    Update settings.py
    removes sensitive info

--- trainer_timetable/settings.py
+++ trainer_timetable/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4_mfo0#nc%_v4!dp(9d=4ia$u-5jlof88fmx7u-8!!qfuhofmo'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
